50e065ce3372a6dd33a453b85f20889f3532d6da
Thomas Guest <thomas.guest@clinithink.com>
2020-04-10 10:51:38 +0100

    Exercise 2.8, added phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 
-const Person = ({name}) => <li>{name}</li>
+const Person = ({person}) => <li>{person.name}, {person.number}</li>
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '123-456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const addPerson = (event) => {
+  const addEntry = (event) => {
     event.preventDefault()
     const found = persons.find(person => person.name === newName)
     if (found)
@@ -17,30 +18,27 @@ const App = () => {
     }
     else
     {
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat(
+        {name: newName, number: newNumber}))
     }
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addEntry}>
+      <div>name: <input value={newName} onChange={handleNameChange}/></div>
+      <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+      <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <Person key={person.name} name={person.name} />)}
+        {persons.map(person => <Person key={person.name} person={person}/>)}
       </ul>
     </div>
   )
