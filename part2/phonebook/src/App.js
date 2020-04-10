b9113763cbe843703bfc6511e97093dda4dd4af0
Thomas Guest <thomas.guest@clinithink.com>
2020-04-10 11:17:01 +0100

    Exercise 2.9, filter entries

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,14 @@ const Person = ({person}) => <li>{person.name}, {person.number}</li>
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const addEntry = (event) => {
     event.preventDefault()
@@ -26,11 +30,13 @@ const App = () => {
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)  
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter entries: <input value={newFilter} onChange={handleFilterChange}/></div>
       <form onSubmit={addEntry}>
       <div>name: <input value={newName} onChange={handleNameChange}/></div>
       <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -38,7 +44,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <Person key={person.name} person={person}/>)}
+        {persons
+          .filter(person => person.name.search(new RegExp(newFilter, 'i')) !== -1)
+          .map(person => <Person key={person.name} person={person}/>)}
       </ul>
     </div>
   )
