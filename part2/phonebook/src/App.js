7cd3c95cfd937a57c073e329414ab8a3a176b111
Thomas Guest <thomas.guest@clinithink.com>
2020-04-10 10:32:12 +0100

    Exercise 2.6, Phonebook application

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const Person = ({name}) => <li>{name}</li>
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Person key={person.name} name={person.name} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
