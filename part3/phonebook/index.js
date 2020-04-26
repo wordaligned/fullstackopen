84e0b87c24cf06cb39f721d03a22535644985997
Thomas Guest <thomas.guest@clinithink.com>
2020-04-26 17:14:22 +0100

    Exercise 3.9, connect phonebook frontend and backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
