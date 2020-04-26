f760e650880a5167f12e985fe2cdba29eca20c0f
Thomas Guest <thomas.guest@clinithink.com>
2020-04-26 08:57:05 +0100

    Exercise 3.3, get person by id

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,7 +27,17 @@ let persons = [
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
 })
 
 app.get('/info', (req, res) => {
