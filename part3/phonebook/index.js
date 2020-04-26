b5e14c9cdb32252437f3ed06713c2f2925fb612e
Thomas Guest <thomas.guest@clinithink.com>
2020-04-26 09:04:01 +0100

    Exercise 3.4, delete api

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
     const content = `
     <p>Phonebook has entries for ${persons.length} people.</p>
