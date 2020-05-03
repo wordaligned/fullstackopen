f70504c70553840744f2d9fc3f09222d978c96a2
Thomas Guest <thomas.guest@clinithink.com>
2020-05-03 09:23:01 +0100

    Exercise 3.16, error handling middleware

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -20,18 +20,26 @@ app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => res.json(persons.map(p => p.toJSON())))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(p => res.json(p.toJSON()))
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON())
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(next)
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(removed => res.status(204).end())
+        .catch(next)
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-
     if (!body.name) {
         return res.status(400).json({ error: 'name missing' })
     }
@@ -52,6 +60,16 @@ app.get('/info', (req, res) => {
     `
     res.send(content)
 })
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
