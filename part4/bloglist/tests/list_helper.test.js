2cf5027e9856b6035e09edcce571a0386d4f464b
Thomas Guest <thomas.guest@clinithink.com>
2020-05-08 17:14:04 +0100

    Exercise 4.5, tests for favourite blog

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -6,3 +6,66 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+const blog1 = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+const blogs3 = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  }
+]
+
+describe('total likes', () => {
+  test('is zero when list is empty', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(blog1)
+    expect(result).toBe(5)
+  })
+  test('is the sum of likes for the listed blogs', () => {
+    const result = listHelper.totalLikes(blogs3)
+    expect(result).toBe(7 + 5 + 12)
+  })
+})
+
+describe('favourite', () => {
+  test('of an empty list is undefined', () => {
+    const result = listHelper.favouriteBlog([])
+    expect(result).toBe(undefined)
+  })
+  test('of a list of one is that one', () => {
+    const result = listHelper.favouriteBlog(blog1)
+    expect(result).toEqual(blog1[0])
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,15 @@
 const dummy = (blogs) => 1
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favouriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    return blogs.reduce((favourite, blog) => {
+      return blog.likes > favourite.likes ? blog : favourite
+    }, blogs[0])
+  }
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog }
