71f7929b47157e54a4f89c1debb591d132d91381
Thomas Guest <thomas.guest@clinithink.com>
2020-05-08 09:36:00 +0100

    Phonebook frontend build

---
+++ part3/phonebook/build/precache-manifest.064bf869157395121ced2070fa9b59dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b7289e11e96c2628444f9f22fd5d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f60f9252c8dc00ac32fe",
+    "url": "/static/css/main.4d6901de.chunk.css"
+  },
+  {
+    "revision": "3f0514d2aaa3dd651ca9",
+    "url": "/static/js/2.79c51fb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79c51fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60f9252c8dc00ac32fe",
+    "url": "/static/js/main.cdadd2cd.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
