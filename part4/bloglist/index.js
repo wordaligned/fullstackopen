20fcbff6f250d6a0c24b99a8c76cedf6f4e35d87
Thomas Guest <thomas.guest@clinithink.com>
2020-05-08 10:00:14 +0100

    Extracted logging

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Blog = require('./models/blog')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -32,5 +33,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
