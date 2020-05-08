89ceb0fc776805a5f06cb6a20e7dfe11c0e703b6
Thomas Guest <thomas.guest@clinithink.com>
2020-05-08 09:56:46 +0100

    Extracted Blog schema

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,17 +2,9 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 require('dotenv').config()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
