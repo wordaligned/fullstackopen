c82b3e0ca00088e01560061d9e3c1d5acc8a3efd
Thomas Guest <thomas.guest@clinithink.com>
2020-05-08 10:32:52 +0100

    Exercise 4.2, extacted controllers, linted

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,37 +1,21 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
