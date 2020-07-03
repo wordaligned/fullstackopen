ab36772e0d326d3cef8f95dde6b78d4341b2fea0
Thomas Guest <thomas.guest@clinithink.com>
2020-07-03 15:53:26 +0100

    Indentation fixes

--- part5/bloglist-frontend/src/components/NewBlogForm.js
+++ part5/bloglist-frontend/src/components/NewBlogForm.js
@@ -18,27 +18,27 @@ const NewBlogForm = ({ createBlog }) => {
     <div>
       <h2>Add new blog</h2>
       <form onSubmit={addBlog}>
-      <div>
-        title 
-        <input
-          type="text" value={title} name="Title"
-          onChange={({ target }) => setTitle(target.value)} />
-      </div>
-      <div>
-        author 
-        <input
-          type="text" value={author} name="Author"
-          onChange={({ target }) => setAuthor(target.value)} />
-      </div>
-      <div>
-        url 
-        <input
-          type="text" value={url} name="Url"
-          onChange={({ target }) => setUrl(target.value)} />
-      </div>
-      <button type="submit">create</button>
-    </form>
-   </div>
+        <div>
+        title
+          <input
+            type="text" value={title} name="Title"
+            onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>
+        author
+          <input
+            type="text" value={author} name="Author"
+            onChange={({ target }) => setAuthor(target.value)} />
+        </div>
+        <div>
+        url
+          <input
+            type="text" value={url} name="Url"
+            onChange={({ target }) => setUrl(target.value)} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
   )
 }
 
