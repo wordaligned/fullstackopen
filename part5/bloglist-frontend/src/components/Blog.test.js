6521f97e060f9fad277e57acff484b6010f62169
Thomas Guest <thomas.guest@clinithink.com>
2020-07-03 15:49:31 +0100

    Exercise 5.15, like button handler test

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,7 +5,7 @@ import { prettyDOM } from '@testing-library/dom'
 
 import Blog from './Blog'
 
-test('renders title and author', () => {
+describe('blog component', () => {
   const blog = {
     title: 'Testing',
     author: 'Tester',
@@ -16,37 +16,43 @@ test('renders title and author', () => {
   const like = jest.fn()
   const remove = jest.fn()
 
-  const component = render(
-    <Blog blog={blog} username='tester' like={like} remove={remove} />
-  )
-  const title = component.container.querySelector('.title')
-  expect(title).toHaveTextContent(blog.title)
+  test('renders title and author', () => {
+    const component = render(
+      <Blog blog={blog} username='tester' like={like} remove={remove} />
+    )
+    const title = component.container.querySelector('.title')
+    expect(title).toHaveTextContent(blog.title)
 
-  const author = component.container.querySelector('.author')
-  expect(author).toHaveTextContent(blog.author)
+    const author = component.container.querySelector('.author')
+    expect(author).toHaveTextContent(blog.author)
 
-  const details = component.container.querySelector('.details')
-  expect(details).toBe(null)
+    const details = component.container.querySelector('.details')
+    expect(details).toBe(null)
+  })
+
+  test('renders details when show is clicked', () => {
+    const component = render(
+      <Blog blog={blog} username='tester' like={like} remove={remove} />
+    )
+    const button = component.getByText('show')
+    fireEvent.click(button)
+    const details = component.container.querySelector('.details')
+    expect(details).toHaveTextContent(blog.url)
+    expect(details).toHaveTextContent('likes')
+  })
+
+  test('like button works', () => {
+    const component = render(
+      <Blog blog={blog} username='tester' like={like} remove={remove} />
+    )
+    const before = like.mock.calls.length
+    fireEvent.click(component.getByText('show'))
+    fireEvent.click(component.getByText('like'))
+    fireEvent.click(component.getByText('like'))
+    const after = like.mock.calls.length
+    expect(after - before).toEqual(2)
+  })
 })
 
-test('renders details when clicked', () => {
-  const blog = {
-    title: 'Testing',
-    author: 'Tester',
-    url: 'http://example.com',
-    user: jest.mock()
-  }
 
-  const like = jest.fn()
-  const remove = jest.fn()
 
-  const component = render(
-    <Blog blog={blog} username='tester' like={like} remove={remove} />
-  )
-  const button = component.getByText('show')
-  fireEvent.click(button)
-
-  const details = component.container.querySelector('.details')
-  expect(details).toHaveTextContent(blog.url)
-  expect(details).toHaveTextContent('likes')
-})
