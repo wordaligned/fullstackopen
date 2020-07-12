ebf3951454b58a4226b0f5eda957b746d983972b
Thomas Guest <thomas.guest@clinithink.com>
2020-07-12 17:06:00 +0100

    Exercise 6.8, extract components

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,39 +1,13 @@
 import React from 'react'
-import { vote, addAnecdote } from './reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import Anecdotes from './components/Anecdotes'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const create = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(addAnecdote(content))
-  }
-
-  const byVote = (a1, a2) => a2.votes - a1.votes
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort(byVote).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={create}>
-        <div><input name='anecdote' /></div>
-        <button type='submit'>create</button>
-      </form>
+      <Anecdotes />
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdotes = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+  const byVote = (a1, a2) => a2.votes - a1.votes
+
+  return (
+    <div>
+      {anecdotes.sort(byVote).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+          </div>
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default Anecdotes
