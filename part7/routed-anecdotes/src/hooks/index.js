c2e567b8264b1065d87c99a85567111e7b96ddf1
Thomas Guest <thomas.guest@clinithink.com>
2020-08-16 17:50:26 +0100

    Exercise 7.5, 7.6 - form reset

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -4,6 +4,7 @@ export const useField = (type) => {
   const [value, setValue] = useState('')
 
   const onChange = event => setValue(event.target.value)
+  const reset = () => setValue('')
 
-  return { type, value, onChange }
+  return { type, value, onChange, reset }
 }
